1582b6e361898456d6015ac156b69e3aa53277b6
Karim Bassem <kimobisso@gmail.com>
2025-07-29 10:20:23 +0300

    the initial commit

---
+++ addons/demo_menu/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Demo Menu',
+    'version': '1.0',
+    'summary': 'A simple module to demonstrate creating a menu and a web page.',
+    'category': 'Website/Website',
+    'author': 'Kbassem10',
+    'website': 'https://www.odoo.com',
+    'license': 'LGPL-3',
+    'depends': ['website'],
+    'data': [
+        'views/student_data.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
